7ceb0c6daddc843e9c0500a8f3f23f1f5e0477b4
webdevpriyank <priyank1500@gmail.com>
2022-11-28 21:40:34 +0530

    webhook done

---
+++ index.js
@@ -0,0 +1,77 @@
+
+"use strict";
+
+const token = process.env.WHATSAPP_TOKEN;
+
+const request = require("request"),
+  express = require("express"),
+  body_parser = require("body-parser"),
+  axios = require("axios").default,
+  app = express().use(body_parser.json()); // creates express http server
+
+
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+app.post("/webhook", (req, res) => {
+
+  let body = req.body;
+
+  console.log(JSON.stringify(req.body, null, 2));
+
+  if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      axios({
+        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Ack: " + msg_body },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    res.sendStatus(200);
+  } else {
+
+    res.sendStatus(404);
+  }
+});
+
+
+app.get("/webhook", (req, res) => {
+
+  const verify_token = process.env.VERIFY_TOKEN;
+
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if (mode && token) {
+
+    if (mode === "subscribe" && token === verify_token) {
+
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+
+    } else {
+
+      res.sendStatus(403);
+
+    }
+  }
+});
